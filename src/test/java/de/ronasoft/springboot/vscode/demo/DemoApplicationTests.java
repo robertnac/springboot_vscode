fe5be0ad7c02f8b93d1b31b1a5e7c41b0ef20848
Robert <robertnac@gmail.com>
2024-03-25 20:27:56 +0100

    Init Commit

---
+++ src/test/java/de/ronasoft/springboot/vscode/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ronasoft.springboot.vscode.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
