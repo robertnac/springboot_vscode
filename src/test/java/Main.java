11a49175f81a8f2094179d7d9bcb7f39095a4c03
Robert <robertnac@gmail.com>
2024-06-16 18:55:31 +0200

    Create Main.java
    Main hinzu

---
+++ src/test/java/Main.java
@@ -0,0 +1,15 @@
+import java.time.LocalTime;
+
+public class Main {
+
+        /**
+     * A description of the entire Java function.
+     *
+     * @param  args	description of parameter
+     * @return         	description of return value
+     */
+    public static void main(String[] args) {
+        LocalTime currentTime = LocalTime.now();
+        System.out.println("Current time: " + currentTime);
+    }
+}
