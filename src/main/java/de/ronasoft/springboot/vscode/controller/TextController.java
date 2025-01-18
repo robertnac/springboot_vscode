a21401e72045fc191c1ec9c4899a74c3685eec46
robertnac <robertnac@gmail.com>
2025-01-18 20:51:44 +0100

    Add  class person with adress

--- src/main/java/de/ronasoft/springboot/vscode/controller/TextController.java
+++ src/main/java/de/ronasoft/springboot/vscode/controller/TextController.java
@@ -2,6 +2,8 @@ package de.ronasoft.springboot.vscode.controller;
 
 import java.time.LocalDateTime;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TextController {
 
+    private static final Logger logger = LoggerFactory.getLogger(TextController.class);
+
+
     @GetMapping("/text")
     public String getText() {
+        logger.debug("Die Methode getText() wurde aufgerufen.");
         return "Hello, the current date and time is: " + LocalDateTime.now();
     }
 }
