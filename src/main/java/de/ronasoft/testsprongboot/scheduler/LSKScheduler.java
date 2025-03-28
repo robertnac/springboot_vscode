bf6b72eb480a1340498339f12d30de6f0fb9f2ff
robertnac <robertnac@gmail.com>
2025-03-28 12:01:06 +0100

    define the cro in application.yml

--- src/main/java/de/ronasoft/testsprongboot/scheduler/LSKScheduler.java
+++ src/main/java/de/ronasoft/testsprongboot/scheduler/LSKScheduler.java
@@ -2,6 +2,7 @@ package de.ronasoft.testsprongboot.scheduler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +15,12 @@ public class LSKScheduler {
     // Create an SLF4J logger instance
     private static final Logger LOGGER = LoggerFactory.getLogger(LSKScheduler.class);
 
-    // Runs once a day at 3 AM
-    @Scheduled(cron = "0 0 3 * * ?")
+    // Inject the cron expression from application.yml
+    @Value("${scheduler.cron}")
+    private String cronExpression;
+
+    // Runs based on the cron expression
+    @Scheduled(cron = "${scheduler.cron}")
     public void scheduledJob() {
         // Get the current time
         LocalDateTime now = LocalDateTime.now();
