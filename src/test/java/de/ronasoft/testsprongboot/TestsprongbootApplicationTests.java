82a48d8335d69e3e71a82bfd4e6327b3bc6240f9
robertnac <robertnac@gmail.com>
2025-04-11 22:28:56 +0200

    Add person and PersonRepo

---
+++ src/test/java/de/ronasoft/testsprongboot/TestsprongbootApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ronasoft.testsprongboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsprongbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Test to ensure the Spring application context loads successfully
+    }
+}
