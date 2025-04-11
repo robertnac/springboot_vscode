3bb6397456a78a5af404a76ac3ababd7e1eaf1c3
robertnac <robertnac@gmail.com>
2025-04-11 22:29:38 +0200

    Add Person entity and PersonRepository for managing person data

---
+++ src/main/java/de/ronasoft/springboot/vscode/entity/Person.java
@@ -0,0 +1,109 @@
+package de.ronasoft.springboot.vscode.entity;
+
+import java.time.LocalDate;
+
+public class Person {
+
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private LocalDate birthdate;
+
+    // Constructors
+    public Person() {
+    }
+
+    public Person(String firstname, String lastname, LocalDate birthdate) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", birthdate=" + birthdate +
+                '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+        result = prime * result + ((lastname == null) ? 0 : lastname.hashCode());
+        result = prime * result + ((birthdate == null) ? 0 : birthdate.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Person other = (Person) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (firstname == null) {
+            if (other.firstname != null)
+                return false;
+        } else if (!firstname.equals(other.firstname))
+            return false;
+        if (lastname == null) {
+            if (other.lastname != null)
+                return false;
+        } else if (!lastname.equals(other.lastname))
+            return false;
+        if (birthdate == null) {
+            if (other.birthdate != null)
+                return false;
+        } else if (!birthdate.equals(other.birthdate))
+            return false;
+        return true;
+    }
+
+}
---
+++ src/main/java/de/ronasoft/springboot/vscode/repository/PersonRepository.java
@@ -0,0 +1,45 @@
+package de.ronasoft.springboot.vscode.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.ronasoft.springboot.vscode.entity.Person;
+
+public class PersonRepository {
+    // create a list of persons
+    private List<Person> persons = new ArrayList<>();
+
+    // Create a new person
+    public void addPerson(Person person) {
+        persons.add(person);
+    }
+
+    // Read all persons
+    public List<Person> getAllPersons() {
+        return new ArrayList<>(persons);
+    }
+
+    // Read a person by ID
+    public Person getPersonById(int id) {
+        return persons.stream()
+                      .filter(person -> person.getId() == id)
+                      .findFirst()
+                      .orElse(null);
+    }
+
+    // Update a person
+    public boolean updatePerson(int id, Person updatedPerson) {
+        for (int i = 0; i < persons.size(); i++) {
+            if (persons.get(i).getId() == id) {
+                persons.set(i, updatedPerson);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Delete a person
+    public boolean deletePerson(int id) {
+        return persons.removeIf(person -> person.getId() == id);
+    }
+}
